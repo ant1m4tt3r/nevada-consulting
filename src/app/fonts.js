d3b9e2104436cb6a71de0a8dcd1db1e04088ba4f
Hugo Azevedo <hugo.soares@outlook.com>
2026-07-30 03:02:43 +0200

    simplify font setup

--- src/app/fonts.js
+++ src/app/fonts.js
@@ -2,6 +2,6 @@ import { Plus_Jakarta_Sans } from 'next/font/google';
 
 export const jakartaSans = Plus_Jakarta_Sans({
   subsets: ['latin'],
-  weight: ['200', '300', '400', '500', '600', '700', '800'],
+  weight: ['400', '500', '600', '700', '800'],
   variable: '--font-jakarta',
 });
